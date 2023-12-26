77705b730c6592dbcdbb4b312d71c89f9bb01466
Tanwarjot Singh <tanwarjot1999@gmail.com>
2023-12-27 00:51:29 +0530

    Understanding the scoping of variable in JavaScript using let, var, const and nun of them (global)

---
+++ Class-5/scoping.js
@@ -0,0 +1,26 @@
+
+var a = "singh";
+function show() {
+    a;
+    console.log(a);
+    
+}
+
+show();
+console.log(a);
+
+
+
+var age = 10;
+
+if (true) {
+    console.log(age);
+}
+console.log("Hi test");
+function display() {
+    let Myname = 'Harry';
+    console.log(Myname.valueOf());
+}
+
+console.log(Myname);
+display();
